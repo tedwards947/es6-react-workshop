986d355e466db50500b11ccb05c81e446140ddab
Tony Edwards <tedwards@netflix.com>
2016-10-05 09:16:04 -0700

    whoops

--- src/client.jsx
+++ src/client.jsx
@@ -1,39 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const INITIAL_NAMES = [
-	'Theresa',
-	'David',
-	'Gordon',
-	'Tony',
-	'John',
-	'Margaret',
-	'James'
-];
-
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            names: INITIAL_NAMES
-        };
-    }
-
-	render() {
-		return (
-			<div>
-				<ol>
-					{this.state.names.map(name => {
-						return <li>{name}</li>
-					})}
-				</ol>
-
-			</div>
-		);
-	}
-}
+import AppRoutes from './components/AppRoutes.jsx';
 
 window.onload = () => {
-	ReactDOM.render(<Home />, document.getElementById('main'));
+  ReactDOM.render(<AppRoutes/>, document.getElementById('main'));
 };
