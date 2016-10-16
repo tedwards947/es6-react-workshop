4034828a98e8a9dc47581f666c5a680e35125c9a
Tony Edwards <tedwards@netflix.com>
2016-10-16 18:32:26 -0500

    setting things up

--- src/client.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRoutes from './components/AppRoutes.jsx';
-
-window.onload = () => {
-  ReactDOM.render(<AppRoutes/>, document.getElementById('main'));
-};
